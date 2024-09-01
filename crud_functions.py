86f07ec3dff33b9468c85e93f064d80799ac2964
dgigur <dgigurda005@mail.ru>
2024-09-01 21:30:36 +0300

    Урок 5

--- crud_functions.py
+++ crud_functions.py
@@ -18,9 +18,35 @@ def initiate_db():
             #f"INSERT INTO Products(title, description, price) VALUES('Продукт{i}', 'Описание{i}', '{i * 100}')")
     connection.commit()
 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    """)
+
+
+def is_included(username):
+    check_user = cursor.execute("SELECT username FROM Users WHERE username = ?",
+                                (username,))
+    if check_user.fetchone() is None:
+        return False
+    return True
+
+
+
 
 def get_all_products():
     cursor.execute("SELECT * FROM Products")
+    connection.commit()
     return cursor.fetchall()
 
+
+def add_user(username, email, age):
+    cursor.execute(f"INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)",
+                   (username, email, age, 1000))
+    connection.commit()
 
